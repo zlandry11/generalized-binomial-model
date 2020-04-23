1d917a2209064e833435676b8d9d2757f4c437b0
Zach Landry <zlandry@haverford.edu>
2020-04-22 22:14:29 -0500

    added create_tree

--- stocktree.py
+++ stocktree.py
@@ -1,10 +1,16 @@
-from binarytree import Node
+from binarytree import Node, build
 
-u = 1.5
-d = .5
-
-root = Node(110)
-root.left = Node(d*root.value)
-root.right = Node(u*root.value)
-
-print('Stock Tree:', root)
+class Tree:
+    
+    def __init__(self, S, u, d):
+        self.S = S
+        self.u = u
+        self.d = d
+        
+    def create_tree(self):
+        tree = [self.S, self.d*self.S, self.u*self.S, self.d**2*self.S, self.d*self.u*self.S, self.d*self.u*self.S, self.u**2*self.S]
+        return build(tree)
+        
+tree = Tree(110, 1.4, .7)
+print(tree.create_tree())
+# Node(self.S, left=Node(self.d*self.S), right=Node(self.u*self.S))
